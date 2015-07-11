b0de8649e4e6cb2af4e52f9ff8daa4b6b96236d7
Routhana Long <deac.long@email.wsu.edu>
2015-07-11 11:55:56 -0700

    Add /restaurants and show list of restaurants

--- webserver.py
+++ webserver.py
@@ -1,12 +1,40 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            if self.path.endswith("/restaurants"):
+
+                restaurants = session.query(Restaurant).all()
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+
+                return
+
+            elif self.path.endswith("/hello"):
+                
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
@@ -19,7 +47,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
-            if self.path.endswith("/hola"):
+            elif self.path.endswith("/hola"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
